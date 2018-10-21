4da3343fb5c1abbe965ebadc400b7c1d84e76fa4
chensheng1005 <ychenyao@qq.com>
2018-10-21 21:51:21 +0800

    -m物流项目

---
+++ Logistics_MyBatis/logistics.sql
@@ -0,0 +1,168 @@
+drop database if exists distribution;
+create database distribution;
+use distribution;
+
+drop table if exists Consumer;
+create table Consumer
+(
+	cId nvarchar(10) primary key not null comment '消费者ID',
+	name nvarchar(10) comment '姓名',
+	sex int comment '性别',
+	userId nvarchar(10) comment '用户组ID',
+	pwd nvarchar(10) comment '密码',
+	phone nvarchar(20) comment '联系电话',
+	birthday datetime comment '出生日期',
+	address nvarchar(50) comment '地址',
+	note nvarchar(50) comment '备注',
+	foreign key(userId) references User(userId)
+)default charset = utf8;
+
+drop table if exists Orders;
+create table Orders
+(
+	oId nvarchar(10) primary key not null comment '订单ID',
+	oType int comment '订单类型',
+	handover nvarchar(10) comment '交接单编号',
+	name nvarchar(20) comment '货物名称',
+	weight int comment '货物重量',
+	youjiren nvarchar(20) comment '邮寄人',
+	maddress nvarchar(20) comment '邮寄地址',
+	saddress nvarchar(20) comment '收货地址',
+	ostate int comment '订单状态',
+	mphone nvarchar(20) comment '邮寄人电话',
+	consignee nvarchar(20) comment '收货人',
+	cphone nvarchar(20) comment '收货电话',
+	startcity nvarchar(10) comment '起始地址',
+	middlecity nvarchar(10) comment '中点地址',
+	endcity nvarchar(10) comment '终点地址',
+	sprice decimal(18,0) comment '运送价格',
+	pprice decimal(18,0) comment '配送价格',
+	yprice decimal(18,0) comment'运输价格',
+	zprice nvarchar(10) comment '总价格',
+	vehicle nvarchar(10) comment '车辆',
+	note nvarchar(50) comment '备注'
+)default charset = utf8;
+
+drop table if exists Vehicle;
+create table Vehicle
+(
+	vId nvarchar(10) primary key not null comment '车辆编号',
+	vmodel nvarchar(10) comment '车辆型号',
+	origin nvarchar(10) comment '产地',
+	route nvarchar(10) comment '路线编号',
+	productionday datetime comment '生产日期',
+	purchaseday datetime comment '购买日期',
+	note nvarchar(50) comment '备注',
+	foreign key(route) references Route(cId)
+)default charset = utf8;
+
+drop table if exists City;
+create table City
+(
+	cId nvarchar(10) primary key not null comment '城市ID',
+	cName nvarchar(10) comment '城市名称',
+	provincesID nvarchar(10) comment '省份ID',
+	foreign key(provincesID) references Provinces(pId)
+)default charset = utf8;
+
+drop table  if exists CityAreas;
+create table CityAreas
+(
+ 	aId nvarchar(10) primary key not null comment '区域ID',
+ 	aName nvarchar(50) comment '区域名称',
+ 	cityID nvarchar(50) comment '城市ID',
+ 	foreign key(cityID) references City(cId)
+)default charset = utf8;
+
+drop table if exists Admin;
+create table Admin
+(
+	adminID nvarchar(10) primary key not null comment '管理员编号',
+	name nvarchar(10) comment '姓名',
+	sex int comment '性别',
+	userID nvarchar(10) comment '用户组编号',
+	astate int comment '状态',
+	acity nvarchar(10) comment '所在城市',
+	address nvarchar(10) comment '地址',
+	pwd nvarchar(10) comment '密码',
+	aphone nvarchar(20) comment '联系电话',
+	note nvarchar(50) comment '备注',
+	foreign key(userID) references User(userId)
+)default charset = utf8;
+
+drop table if exists Receipt;
+create table Receipt
+(
+	startcity nvarchar(10) primary key not null comment '起点城市',
+	endcity nvarchar(10) comment '终点城市',
+	arrivedcity nvarchar(10) comment '已到城市',
+	cstate int comment '状态',
+	generateday datetime comment '生成时间',
+	note nvarchar(50) comment '备注'
+)default charset = utf8;
+
+drop table if exists Route;
+create table Route
+(
+	cId nvarchar(10) primary key not null comment '路线编号',
+	cname nvarchar(10) comment '路线名称',
+	pstartId nvarchar(10) comment '起始省份编号',
+	cstartId nvarchar(10) comment '起始城市编号',
+	pmiddleId nvarchar(10) comment '中点省份编号',
+	cmiddleId nvarchar(10) comment '中点城市编号',
+	pendId nvarchar(10) comment '终点省份编号',
+	cendId nvarchar(10) comment '终点城市编号',
+	yprice decimal(18,0) comment '运输价格',
+	note nvarchar(50) comment '备注',
+	foreign key(pstartId) references Provinces(pId),
+	foreign key(pmiddleId) references Provinces(pId),
+	foreign key(pendId) references Provinces(pId),
+	foreign key(cstartId) references City(cId),
+	foreign key(cmiddleId) references City(cId),
+	foreign key(cendId) references City(cId)
+)default charset = utf8;
+
+drop table if exists Distribution;
+create table Distribution
+(
+	dId int primary key not null comment '配送点ID',
+	dname nvarchar(10) comment '配送点名称',
+	pid nvarchar(10) comment '省份ID',
+	cid nvarchar(10) comment '城市ID',
+	aid nvarchar(10) comment '管理员ID',
+	phone nvarchar(20) comment '联系电话',
+	address nvarchar(50) comment '地址',
+	note nvarchar(50) comment '备注',
+	foreign key(pid) references Provinces(pId),
+	foreign key(cid) references City(cId),
+	foreign key(aid) references Admin(adminID)
+)default charset = utf8;
+
+drop table if exists DisScope;
+create table DisScope
+(
+	areasId int primary key not null comment '配送区域ID',
+	areasname nvarchar(10) comment '配送区域名称',
+	aprice decimal(18,0) comment '配送价格',
+	cId nvarchar(10) comment '所属城市ID',
+	note nvarchar(50) comment '备注',
+	foreign key(cId) references City(cId)
+)default charset = utf8;
+
+drop table if exists Provinces;
+create table Provinces
+(
+	pId nvarchar(10) primary key not null comment '省份ID',
+	pname nvarchar(10) comment '省份名称'
+)default charset = utf8;
+
+drop table if exists User;
+create table User
+(
+	userId nvarchar(10) primary key not null comment '用户组编号',
+	username nvarchar(10) not null comment '用户组名称',
+	note nvarchar(50) comment '备注'
+)default charset = utf8;
+
+
+
